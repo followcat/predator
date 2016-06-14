cea7df09c01b0324e080b469387ac8782eb5bfb0
followcat <followcat@163.com>
2016-06-14 17:50:04 +0800

    Remove module downloader tools.

--- downloader/liepin.py
+++ downloader/liepin.py
@@ -1,7 +1,11 @@
-
-import downloader.tools
 import downloader.urllib
 
+
+def getcookies():
+    cookies_str = ''
+    with open('cookies.data') as fp:
+        cookies_str = fp.read()
+    return cookies_str
 
 def classify_postdata(update_dict):
     post_data = {
@@ -26,14 +30,14 @@ def classify_postdata(update_dict):
     return post_data
 
 def classify_search(data):
-    cookies_str = downloader.tools.getcookies()
+    cookies_str = getcookies()
     dl_url = downloader.urllib.Urllib()
     dl_url.set_cookies(cookies_str)
     searchurl = 'https://h.liepin.com/cvsearch/soResume/'
     return dl_url.post(searchurl, data=data)
 
 def cv(cv_url):
-    cookies_str = downloader.tools.getcookies()
+    cookies_str = getcookies()
     dl_url = downloader.urllib.Urllib()
     dl_url.set_cookies(cookies_str)
     download_url = 'https://h.liepin.com' + cv_url
--- downloader/tools.py
+++
@@ -1,5 +0,0 @@
-def getcookies():
-    cookies_str = ''
-    with open('cookies.data') as fp:
-        cookies_str = fp.read()
-    return cookies_str
