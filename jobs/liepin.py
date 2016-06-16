48d731f5226928af5da35df6b1fdca2033e423f6
followcat <followcat@163.com>
2016-06-16 17:55:22 +0800

    Modify liepin plan one to run each 5 minutes.

--- jobs/liepin.py
+++ jobs/liepin.py
@@ -7,6 +7,6 @@ FF_PROFILE_PATH = '/home/followcat/.mozilla/firefox/yffp11op.followcat'
 PRECEDURE_CLASS = precedure.liepin.Liepin
 YAMLDATA = utils.builtin.load_yaml('liepin/JOBTITLES', '290097.yaml')
 
-PLAN = [dict(minute='*/1', hour='8-17'),
+PLAN = [dict(minute='*/5', hour='8-17'),
         dict(minute='*/15', hour='18-23'),
         dict(minute='*/15', hour='0-2')]
