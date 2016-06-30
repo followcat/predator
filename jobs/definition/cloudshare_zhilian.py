7d26024400ec732b820e6a9eeedbef602ce47a0f
followcat <followcat@163.com>
2016-06-30 16:18:40 +0800

    Modify Zhilian to job definition Cloudshare subclass.

--- jobs/definition/cloudshare_zhilian.py
+++ jobs/definition/cloudshare_zhilian.py
@@ -6,53 +6,23 @@ import functools
 
 import pypandoc
 
-import jobs.definition.base
-import storage.cv
 import utils.builtin
 import precedure.zhilian
-import storage.fsinterface
-import storage.jobtitles
-import downloader.webdriver
+import jobs.definition.cloudshare
 
 from extractor.extract_experience import *
 from extractor.information_explorer import *
 
 
-class Zhilian(jobs.definition.base.Base):
+class Zhilian(jobs.definition.cloudshare.Cloudshare):
 
     CVDB_PATH = 'output/zhilian'
     FF_PROFILE_PATH = '/home/kabess/.mozilla/firefox/g648khbx.default'
     PRECEDURE_CLASS = precedure.zhilian.Zhilian
 
-
-
-    def __init__(self):
-        self.wb_downloader = downloader.webdriver.Webdriver(self.FF_PROFILE_PATH)
-        self.precedure = self.PRECEDURE_CLASS(wbdownloader=self.wb_downloader)
-        self.fsinterface = storage.fsinterface.FSInterface(self.CVDB_PATH)
-        self.cvstorage = storage.cv.CurriculumVitae(self.fsinterface)
-        self.jtstorage = storage.jobtitles.JobTitles(self.fsinterface)
-
     def cloudshare_yaml_template(self):
-        template = {
-            'age': 0,
-            'comment': [],
-            'committer': "SCRAPPY",
-            'company': "",
-            'date': 0.,
-            'education': "",
-            'email': '',
-            'experience': [],
-            'filename': "",
-            'id': '',
-            'name': "",
-            'origin': u'智联卓聘爬取',
-            'phone': "",
-            'position': "",
-            'school': "",
-            'tag': [],
-            'tracking': [],
-            }
+        template = super(Liepin, self).cloudshare_yaml_template()
+        template['origin'] = u'智联卓聘爬取'
         return template
 
     def jobgenerator(self, industry_yamls):
@@ -76,13 +46,13 @@ class Zhilian(jobs.definition.base.Base):
         print('Download: '+cv_id)
         try:
             cv_content =  self.precedure.cv(cv_info['href'])
-            cvresult = self.cvstorage.add(cv_id, cv_content.encode('utf-8'))
+            cvresult = True
         except precedure.zhilian.NocontentCVException:
             print('Failed! Download: '+cv_id)
             cvresult = False
         if cvresult is True:
             yamldata = self.extract_details(cv_info, cv_content)
-            jtresult = self.jtstorage.add_data(cv_id, yamldata)
+            cvresult = self.cvstorage.addcv(cv_id, cv_content.encode('utf-8'), yamldata)
             job_logger.info('Download: '+cv_id)
         else:
             job_logger.info('Failed! Download: '+cv_id)
@@ -110,31 +80,5 @@ class Zhilian(jobs.definition.base.Base):
             details['major'] = education[2]
 
         md = pypandoc.convert(cv_content, 'markdown', format='docbook')
-        experiences = []
-        extracted_data = fix(md)
-        RE = re.compile(DURATION)
-        if not extracted_data[1]:
-            (company, position) = extracted_data[0]
-            for (i,c) in enumerate(company):
-                current_positions = [p for p in position if p[4] == i]
-                try:
-                    if details['position'] == '':
-                        details['position'] = current_positions[0][2]
-                    if details['company'] == '':
-                        details['company'] = company[0][2]
-                except IndexError:
-                    pass
-                for p in current_positions:
-                    if c[3] and len(current_positions) == 1 and not RE.search(p[2]):
-                        experiences.append((p[0], p[1], c[2]+'|'+p[2]+'('+c[3]+')'))
-                    elif c[3]:
-                        experiences.append((p[0], p[1], c[2]+'('+c[3]+')'+'|'+p[2]))
-                    else:
-                        experiences.append((p[0], p[1], c[2]+'|'+p[2]))
-                else:
-                    if not len(current_positions):
-                        experiences.append((c[0], c[1], c[2]))
-            details['experience'] = experiences
-            if u'…' in details['company']:
-                details['company'] = company[0]
+        details.update(get_experience(md))
         return details
