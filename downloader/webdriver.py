a14423c3c31bf29809bce97f73b9151d9a4e5047
winky <winky@debian>
2016-07-29 15:12:30 +0800

    modified function 'close()' to 'quit()'

--- downloader/webdriver.py
+++ downloader/webdriver.py
@@ -8,8 +8,11 @@ class Webdriver(object):
             profile = None
         else:
             profile = selenium.webdriver.FirefoxProfile(profilepath)
-        self.driver =  selenium.webdriver.Firefox(firefox_profile=profile)
+        self.driver =  selenium.webdriver.Firefox(firefox_profile=profile, timeout=600)
 
     def getsource(self, url):
         self.driver.get(url)
         return self.driver.page_source
+
+    def close(self):
+        self.driver.quit()
