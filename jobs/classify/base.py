f6fbd31cc171c468144b8073ec2b798a2a3a66d8
followcat <followcat@163.com>
2016-08-03 10:47:56 +0800

    Add classify jobs base class, use job generator to get job and run from plan.

---
+++ jobs/classify/base.py
@@ -0,0 +1,22 @@
+import utils.builtin
+import storage.jobtitles
+import downloader._urllib
+import downloader.webdriver
+
+
+def Base(object):
+
+    ff_profile = None
+    cookies_file = None
+
+    def __init__(self, interface):
+        if cookies_file is not None:
+            self.cookies_str = utils.builtin.loadfile('cookies.data')
+            self.downloader = downloader._urllib.Urllib()
+            self.downloader.set_cookies(self.cookies_str)
+        elif ff_profile is not None:
+            self.downloader = downloader.webdriver.Webdriver(self.ff_profile)
+        self.repojt = storage.jobtitles.JobTitles(interface)
+
+    def jobgenerator(self):
+        pass
--- jobs/classify/liepin.py
+++ jobs/classify/liepin.py
@@ -1,33 +1,39 @@
-import utils.builtin
+import functools
+
 import precedure.liepin
-import downloader._urllib
-import storage.jobtitles
+import jobs.classify.base
 import storage.gitinterface
 
 from sources.liepin import localdatajobs
 
 
-cookies_str = utils.builtin.loadfile('cookies.data')
-ul_downloader = downloader._urllib.Urllib()
-ul_downloader.set_cookies(cookies_str)
-liepin = precedure.liepin.Liepin(uldownloader=ul_downloader)
+class Liepin(jobs.classify.base.Base):
+
+    cookies_file = 'cookies.data'
+
+    def jobgenerator(self):
+        liepin = precedure.liepin.Liepin(uldownloader=self.downloader)
+        selected_list = [
+        '290094', #医疗器械研发
+        '290097', #医疗器械生产/质量管理
+        '060010', #市场总监
+        '020010', #销售总监
+        '040020', #项目经理/主管
+        '040040', #项目专员/助理
+        '050010', #质量管理/测试经理(QA/QC经理)
+        '050080', #体系工程师/审核员
+        '050020', #质量管理/测试主管(QA/QC主管)
+        ]
+        for id_str in selected_list:
+            print localdatajobs['jobtitles'][id_str][0]
+            postdict = {'industrys': 290,
+                        'jobtitles': id_str,
+                        'curPage': 0}
+            job_process = functools.partial(liepin.update_classify, postdict, self.repojt)
+            yield job_process
+
 
 repo = storage.gitinterface.GitInterface('liepin')
-repojt = storage.jobtitles.JobTitles(repo)
-selected_list = [
-'290094', #医疗器械研发
-'290097', #医疗器械生产/质量管理
-'060010', #市场总监
-'020010', #销售总监
-'040020', #项目经理/主管
-'040040', #项目专员/助理
-'050010', #质量管理/测试经理(QA/QC经理)
-'050080', #体系工程师/审核员
-'050020', #质量管理/测试主管(QA/QC主管)
-]
-for id_str in selected_list:
-    print localdatajobs['jobtitles'][id_str][0]
-    postdict = {'industrys': 290,
-                'jobtitles': id_str,
-                'curPage': 0}
-    liepin.update_classify(postdict, repojt)
+instance = Liepin(repo)
+PROCESS_GEN = instance.jobgenerator()
+PLAN = [dict(minute='*/5')]
