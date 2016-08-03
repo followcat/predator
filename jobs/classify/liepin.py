2a6c61b66bdc529eb7d53ea3c3d48ffa84747a02
followcat <followcat@163.com>
2016-08-03 17:55:20 +0800

    Modify liepin selected_list.

--- jobs/classify/liepin.py
+++ jobs/classify/liepin.py
@@ -4,8 +4,6 @@ import precedure.liepin
 import jobs.classify.base
 import storage.gitinterface
 
-from sources.liepin import localdatajobs
-
 
 class Liepin(jobs.classify.base.Base):
 
@@ -13,19 +11,19 @@ class Liepin(jobs.classify.base.Base):
 
     def jobgenerator(self):
         liepin = precedure.liepin.Liepin(uldownloader=self.downloader)
-        selected_list = [
-        '290094', #医疗器械研发
-        '290097', #医疗器械生产/质量管理
-        '060010', #市场总监
-        '020010', #销售总监
-        '040020', #项目经理/主管
-        '040040', #项目专员/助理
-        '050010', #质量管理/测试经理(QA/QC经理)
-        '050080', #体系工程师/审核员
-        '050020', #质量管理/测试主管(QA/QC主管)
-        ]
+        selected_list = {
+            '290094': u'医疗器械研发',
+            '290097': u'医疗器械生产/质量管理',
+            '060010': u'市场总监',
+            '020010': u'销售总监',
+            '040020': u'项目经理/主管',
+            '040040': u'项目专员/助理',
+            '050010': u'质量管理/测试经理(QA/QC经理)',
+            '050080': u'体系工程师/审核员',
+            '050020': u'质量管理/测试主管(QA/QC主管)',
+        }
         for id_str in selected_list:
-            print localdatajobs['jobtitles'][id_str][0]
+            print selected_list[id_str]
             postdict = {'industrys': 290,
                         'jobtitles': id_str,
                         'curPage': 0}
