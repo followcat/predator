836712f6b76e6f12679b08614544c16575056b35
winky <454037661@qq.com>
2016-08-16 17:27:09 +0800

    Add a condition to judge whether the tags is already in result

--- precedure/base.py
+++ precedure/base.py
@@ -49,7 +49,7 @@ class Base(object):
         for cur_page in range(self.CLASSIFY_MAXPAGE):
             postdict[self.PAGE_VAR] = cur_page + 1
             try:
-                results = self.classify(postdict)
+                results = self.classify(postdict, header)
             except Exception:
                 break
             if not results:
@@ -58,11 +58,17 @@ class Base(object):
             for result in results:
                 if not repojt.exists(id_str, result['id']):
                     parts_results.append(result)
+                else:
+                    for key in header['tags'].keys():
+                        if header['tags'][key] not in result['tags'][key]:
+                             result['tags'][key].add(header['tags'][key])
             print 'current page:' + str(cur_page + 1)
             if len(parts_results) < len(results)*0.2:
                 break
             else:
                 add_list.extend(parts_results)
             time.sleep(self.CLASSIFY_SLEEP)
-        repojt.add_datas(filename, add_list, header)
+        head = {}
+        head['postdict'] = header['postdict']
+        repojt.add_datas(filename, add_list, head)
         return True
