7549903843416a1e9c238f826a534c9c3df4bcd1
winky <454037661@qq.com>
2016-08-17 11:42:14 +0800

    Modify liepin crawler to fit the new industry id

--- jobs/cv/liepin.py
+++ jobs/cv/liepin.py
@@ -3,7 +3,7 @@ import jobs.definition.cloudshare_liepin
 
 instance = jobs.definition.cloudshare_liepin.Liepin()
 
-PROCESS_GEN = instance.jobgenerator('290097')
+PROCESS_GEN = instance.jobgenerator()
 PLAN = [dict(minute='*/5', hour='8-17'),
         dict(minute='*/15', hour='18-23'),
         dict(minute='*/15', hour='0-2')]
