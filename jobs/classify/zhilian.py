90370cce6e82b09d2a07cf522ea2db999214ab24
followcat <followcat@163.com>
2016-08-22 18:37:08 +0800

    Modify zhilian to support classify resume download.

--- jobs/classify/zhilian.py
+++ jobs/classify/zhilian.py
@@ -22,17 +22,16 @@ class Zhilian(jobs.classify.base.Base):
             industry = industry.encode('utf-8')
             industryid = industryID[industry]
             zhilian_industry = industry_dict[industry]['zhilian']
-            if len(zhilian_industry) == 0:
-                continue
+            filename = industryid
+            temp_resume = resume
             for index in zhilian_industry:
                 industry_id = index[0]
                 industry_value = index[1]
-                filename = industryid
                 print '抓取的行业：' + industry_value
                 postinfo = {
                     'industrys': industry_value
                             }
-                for job_key in jobtype_list.keys():
+                for job_key in sorted(jobtype_list.keys()):
                     job_type = jobtype_list[job_key].encode('utf-8')
                     print "正在抓取的职位: " + job_type
                     postinfo['jobtitles'] = job_type
@@ -42,9 +41,13 @@ class Zhilian(jobs.classify.base.Base):
                         }
                     header = self.gen_header(paramsdict, postinfo)
                     print header
+                    if temp_resume and not self.eq_postdict(industryid, paramsdict):
+                        continue
+                    else:
+                        temp_resume = False
                     job_process = functools.partial(zhilian.update_classify,
                                                 filename, filename,
-                                                 paramsdict, self.repojt,header)
+                                                 paramsdict, self.repojt, header)
                     yield job_process
 
 repo = storage.gitinterface.GitInterface('zhilian')
