c59e8da5d7dd446ee601fedfea7420d79a6348d5
followcat <followcat@163.com>
2016-08-30 12:28:02 +0800

    Remove all config in jobs yingcai config.

--- jobs/config/yingcai.py
+++ jobs/config/yingcai.py
@@ -1,7 +1,3 @@
-accounts = [('454037661@qq.com', 'qinko10030729'),
-            ('wanqin@willendare.com', 'qinko10030729'),
-            ('18244990301', 'qinko10030729')]
+accounts = []
 
-ff_profiles = ['/home/winky/.mozilla/firefox/4idae7tm.winky3',
-               '/home/winky/.mozilla/firefox/jvqqz5ch.winky',
-               '/home/winky/.mozilla/firefox/bs9yw52t.winky2']
+ff_profiles = []
