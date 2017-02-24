6d0b09ab28fb66ab63b8a0560c1b177475fbd8ad
jeff <jeff@debian83>
2017-02-24 12:52:07 +0800

    Rebuild the web driver if it timeout

--- downloader/webdriver.py
+++ downloader/webdriver.py
@@ -1,17 +1,28 @@
+import socket
 import selenium.webdriver
 
 
 class Webdriver(object):
 
     def __init__(self, profilepath=None):
-        if profilepath is None:
-            profile = None
-        else:
-            profile = selenium.webdriver.FirefoxProfile(profilepath)
+        self.profilepath = profilepath
+        profile = self.create_ff_profile()
         self.driver =  selenium.webdriver.Firefox(firefox_profile=profile, timeout=600)
 
+    def create_ff_profile(self):
+        if self.profilepath is None:
+            profile = None
+        else:
+            profile = selenium.webdriver.FirefoxProfile(self.profilepath)
+
     def getsource(self, url):
-        self.driver.get(url)
+        try:
+            self.driver.get(url)
+        except socket.error:
+            self.driver.quit()
+            profile = self.create_ff_profile()
+            self.driver = selenium.webdriver.Firefox(firefox_profile=profile, timeout=600)
+            self.driver.get(url)
         return self.driver.page_source
 
     def close(self):
