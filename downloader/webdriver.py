3a2f32a8c20fac2870fe43e8d11396731d8f0a48
jeff <jeff@debian83>
2017-02-24 17:15:03 +0800

    Move firefox driver creating out of class definition

--- downloader/webdriver.py
+++ downloader/webdriver.py
@@ -2,28 +2,27 @@ import socket
 import selenium.webdriver
 
 
-def create_ff_profile(profile_path):
+def create_firefox_driver(profile_path):
     if profile_path is None:
         profile = None
     else:
         profile = selenium.webdriver.FirefoxProfile(profile_path)
-    return profile
+    driver =  selenium.webdriver.Firefox(firefox_profile=profile, timeout=600)
+    return driver
 
 
 class Webdriver(object):
 
     def __init__(self, profilepath=None):
         self.profilepath = profilepath
-        profile = create_ff_profile(self.profilepath)
-        self.driver =  selenium.webdriver.Firefox(firefox_profile=profile, timeout=600)
+        self.driver =  create_firefox_driver(self.profilepath)
 
     def getsource(self, url):
         try:
             self.driver.get(url)
         except socket.error:
             self.driver.quit()
-            profile = create_ff_profile(self.profilepath)
-            self.driver = selenium.webdriver.Firefox(firefox_profile=profile, timeout=600)
+            self.driver =  create_firefox_driver(self.profilepath)
             self.driver.get(url)
         return self.driver.page_source
 
