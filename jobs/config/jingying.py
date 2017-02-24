c61fb0cfc5d8f4e541c7eeafc5e4b20032996338
jeff <jeff@debian83>
2017-02-24 16:20:00 +0800

    Add profile info config file for jingying

---
+++ jobs/config/jingying.py
@@ -0,0 +1,6 @@
+ff_profiles = [
+                '/home/jeff/.mozilla/firefox/16xuwjcx.51jingying_2',
+                '/home/jeff/.mozilla/firefox/u2ip99rl.flj_jingying',
+                '/home/jeff/.mozilla/firefox/2mkrz29h.jingying_chunjing',
+                '/home/jeff/.mozilla/firefox/8xjhortl.jingying_jingwen',
+    ]
