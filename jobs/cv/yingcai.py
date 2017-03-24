1eeafe19ee611c7045a27ec3e63ec0e745a4281a
jeff <jeff@debian83>
2017-03-24 15:24:44 +0800

    Update the yingcai cv job to share downloader

--- jobs/cv/yingcai.py
+++ jobs/cv/yingcai.py
@@ -1,9 +1,14 @@
 import jobs.definition.cloudshare_yingcai
 
 
-instance = jobs.definition.cloudshare_yingcai.Yingcai()
 
-PROCESS_GEN_FUNC = instance.jobgenerator
-PLAN = [dict(name='yingcai_cv', second='*/60', hour='8-17'),
-        dict(name='yingcai_cv', second='*/60', hour='18-23'),
+def get_PROCESS_GEN_FUNC(downloaders=None):
+    instance = jobs.definition.cloudshare_yingcai.Yingcai(wbdownloaders=downloaders)
+    if instance.source not in downloaders:
+        downloaders[instance.source] = instance.precedure.wb_downloader
+    PROCESS_GEN_FUNC = instance.jobgenerator
+    return PROCESS_GEN_FUNC
+
+PLAN = [dict(name='yingcai_cv', second='*/60', hour='8-19'),
+        dict(name='yingcai_cv', second='*/60', hour='20-23'),
         dict(name='yingcai_cv', second='*/60', hour='0-7')]
