835834696db758458e13b403a5f4ed44550122a5
jeff <jeff@debian83>
2017-04-07 10:46:28 +0800

    Add get_setting function in base class

--- jobs/base.py
+++ jobs/base.py
@@ -1,3 +1,4 @@
+import utils.builtin
 import downloader.webdriver
 
 
@@ -10,5 +11,26 @@ class Base(object):
             return downloader.webdriver.Webdriver(downloader=wbdownloader)
         return downloader.webdriver.Webdriver(profile_path)
 
-
-
+    def get_setting(self, config):
+        settings = {}
+        path = config[:config.rindex('/')]
+        cfile = config[config.rindex('/')+1:]
+        contents = utils.builtin.load_yaml(path, cfile)
+        for k, v in contents.items():
+            ind_file = v['industries']
+            kw_file = v['keywords']
+            inds = []
+            kws = []
+            with open(ind_file) as ind_f:
+                for l in ind_f.readlines():
+                    l = l.strip()
+                    if l.startswith('#'):
+                        continue
+                    inds.append(l)
+            with open(kw_file) as kw_f:
+                for kw in kw_f.readlines():
+                    if l.startswith("#"):
+                        continue
+                    kws.append(l.decode('utf-8'))
+            settings[k] = (inds, kws)
+        return settings
