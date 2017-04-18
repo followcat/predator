6f31672aa770f7eedf2f3336ba27b54a6de46be8
jeff <jeff@debian83>
2017-04-18 12:11:02 +0800

    Return instance from function return for share webdriver

--- jobs/cv/jingying.py
+++ jobs/cv/jingying.py
@@ -1,9 +1,13 @@
 import jobs.definition.cloudshare_jingying
 
 
-instance = jobs.definition.cloudshare_jingying.Jingying()
+def get_PROCESS_GEN_FUNC(downloaders=None):
+    instance = jobs.definition.cloudshare_jingying.Jingying(wbdownloaders=downloaders)
+    if instance.source not in downloaders:
+        downloaders[instance.source] = instance.precedure.wb_downloader
+    PROCESS_GEN_FUNC = instance.jobgenerator
+    return PROCESS_GEN_FUNC
 
-PROCESS_GEN_FUNC = instance.jobgenerator
 PLAN = [dict(name='jingying_cv', second='*/10', hour='8-17'),
         dict(name='jingying_cv', second='*/60', hour='18-23'),
         dict(name='jingying_cv', second='*/60', hour='0-7')]
