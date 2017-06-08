e48f6b257a543a5aab0a35e3f9a8c56c5b7934c1
followcat <followcat@163.com>
2017-06-08 16:46:46 +0800

    Update jingying the newest date to 14 day.

--- jobs/definition/cloudshare_jingying.py
+++ jobs/definition/cloudshare_jingying.py
@@ -37,7 +37,7 @@ class Jingying(jobs.definition.cloudshare.Cloudshare):
                                reverse=True)
             print _file, sorted_id[0], time.localtime(yamldata[sorted_id[0]]['date'])
             for cv_id in sorted_id:
-                if (time.time() - yamldata[cv_id]['date'])/60/60/24 < 7:# not self.cvstorage.existscv(cv_id):
+                if (time.time() - yamldata[cv_id]['date'])/60/60/24 < 14:# not self.cvstorage.existscv(cv_id):
                     cv_info = yamldata[cv_id]
                     job_process = functools.partial(self.downloadjob, cv_info, _classify_id)
                     yield job_process
