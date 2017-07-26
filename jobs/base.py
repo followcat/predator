87a6883422a91b3f38bd2cf8cbb0f24f72be534a
followcat <followcat@163.com>
2017-07-26 13:09:05 +0800

    Modify get_setting in jobs base.

--- jobs/base.py
+++ jobs/base.py
@@ -21,21 +21,20 @@ class Base(object):
         if config != '':
             contents = utils.builtin.load_yaml('', config)
             for k, v in contents.items():
-                ind_file = v['industries']
-                kw_file = v['keywords']
-                kw_path = v['keywordpath']
-                inds = utils.builtin.load_yaml('', ind_file)
-                kws_dict = utils.builtin.load_yaml('', kw_file)
                 kws = []
-                for filepath in glob.glob(os.path.join(kw_path, '*.yaml')):
-                    kpt_dict = utils.builtin.load_yaml(*os.path.split(filepath))
-                    for pos in kpt_dict:
-                        for keywords_list in pos.values():
-                            for keywords in keywords_list:
-                                if len(keywords.split(' ')) < 7 and keywords:
-                                    kws.append(keywords)
-                for _k, _v in kws_dict.items():
-                    kws.extend(_v)
+                inds = utils.builtin.load_yaml('', ind_file)
+                ind_file = v['industries']
+                if 'keywords' in v:
+                    for _k, _v in utils.builtin.load_yaml('', v['keywords']).items():
+                        kws.extend(_v)
+                if 'keywordpath' in v:
+                    for filepath in glob.glob(os.path.join(v['keywordpath'], '*.yaml')):
+                        kpt_dict = utils.builtin.load_yaml(*os.path.split(filepath))
+                        for pos in kpt_dict:
+                            for keywords_list in pos.values():
+                                for keywords in keywords_list:
+                                    if len(keywords.split(' ')) < 7 and keywords:
+                                        kws.append(keywords)
                 settings.append((inds, kws))
         default_keywords = []
         settings.append((default_industries, default_keywords))
