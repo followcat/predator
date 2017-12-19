cddb0b65cfbd83350a4281c5e7e94351bc304c65
followcat <followcat@163.com>
2017-12-19 16:56:53 +0800

    Update plugins client content, support no image page.

--- plugins/client/content.js
+++ plugins/client/content.js
@@ -5,6 +5,7 @@ function onWindowLoad() {
     var html = document.documentElement.outerHTML;
     var email = document.getElementsByClassName('email')[0];
     var telephone = document.getElementsByClassName('telphone')[0];
+
     var base64email = getBase64Image(email);
     var base64phone = getBase64Image(telephone);
     sendMsg(url, html, base64email, base64phone, "end");
@@ -20,19 +21,24 @@ function sendMsg(url, html, base64email, base64phone, cmd){
 
 function getBase64Image(img) {
     // Create an empty canvas element
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
+    if (img) {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-    // Copy the image contents to the canvas
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
+        // Copy the image contents to the canvas
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
 
-    // Get the data-URL formatted image
-    // Firefox supports PNG and JPEG. You could check img.src to
-    // guess the original format, but be aware the using "image/jpg"
-    // will re-encode the image.
-    var dataURL = canvas.toDataURL("image/png");
+        // Get the data-URL formatted image
+        // Firefox supports PNG and JPEG. You could check img.src to
+        // guess the original format, but be aware the using "image/jpg"
+        // will re-encode the image.
+        var dataURL = canvas.toDataURL("image/png");
 
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    }
+    else {
+        return '';
+    }
 }
